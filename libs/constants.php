9174437cd0d996b7f95650b1acd4c60c53bbb9f9
Nebojša Jakovljević <nebjak@gmail.com>
2012-02-25 11:09:35 +0100

    fixed tabs, and an html error

--- libs/constants.php
+++ libs/constants.php
@@ -1,10 +1,9 @@
 <?php 
-	define(HOME_TITLE, 'NV Color - Novi Sad - Molersko-farbarski i gipsani radovi');
-	define(PONUDA_TITLE, 'Ponuda - NV Color - Novi Sad');
-	define(GALERIJA_TITLE, 'Galerija - NV Color - Novi Sad');
-	define(KONTAKT_TITLE, 'Kontakt - NV Color - Novi Sad');
-	define(HEADER_FILE, 'libs/header.php');
-	define(FOOTER_FILE, 'libs/footer.php');
-	define(MAIN_MENU_FILE, 'libs/main_menu.php');
+  define(HOME_TITLE, 'NV Color - Novi Sad - Molersko-farbarski i gipsani radovi');
+  define(PONUDA_TITLE, 'Ponuda - NV Color - Novi Sad');
+  define(GALERIJA_TITLE, 'Galerija - NV Color - Novi Sad');
+  define(KONTAKT_TITLE, 'Kontakt - NV Color - Novi Sad');
+  define(HEADER_FILE, 'libs/header.php');
+  define(FOOTER_FILE, 'libs/footer.php');
+  define(MAIN_MENU_FILE, 'libs/main_menu.php');
 ?>
-
